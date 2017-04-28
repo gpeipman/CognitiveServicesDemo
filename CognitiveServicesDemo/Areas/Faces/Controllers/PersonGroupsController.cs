275d620dbda2b652c96497028c400325a5439514
Gunnar <gpeipman@hotmail.com>
2017-04-29 00:33:20 +0300

    displaying errors on person group creating view

--- CognitiveServicesDemo/Areas/Faces/Controllers/PersonGroupsController.cs
+++ CognitiveServicesDemo/Areas/Faces/Controllers/PersonGroupsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Microsoft.ProjectOxford.Face;
 using Microsoft.ProjectOxford.Face.Contract;
 
 namespace CognitiveServicesDemo.Areas.Faces.Controllers
@@ -43,9 +44,14 @@ namespace CognitiveServicesDemo.Areas.Faces.Controllers
                     await client.CreatePersonGroupAsync(model.PersonGroupId, model.Name, model.UserData);
                 }
             }
+            catch (FaceAPIException fex)
+            {
+                ModelState.AddModelError(string.Empty, fex.ErrorMessage);
+                return View(model);
+            }
             catch (Exception ex)
             {
-                ModelState.AddModelError("_FORM", ex);
+                ModelState.AddModelError(string.Empty, ex.Message);
                 return View(model);
             }
 
