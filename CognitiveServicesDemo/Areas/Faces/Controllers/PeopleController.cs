5f6c975c3d064d7649900a521575d9d01be83079
Gunnar <gpeipman@hotmail.com>
2017-04-30 18:57:50 +0300

    added controller with list, create and edit for people

---
+++ CognitiveServicesDemo/Areas/Faces/Controllers/PeopleController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+
+namespace CognitiveServicesDemo.Areas.Faces.Controllers
+{
+    public class PeopleController : FacesBaseController
+    {
+        public async Task<ActionResult> Index(string id)
+        {
+            if(string.IsNullOrEmpty(id))
+            {
+                return HttpNotFound("Person group ID is missing");
+            }
+
+            using (var client = GetFaceClient())
+            {
+                var model = await client.GetPersonsAsync(id);
+                ViewBag.PersonGroupId = id;
+
+                return View(model);
+            }
+        }
+
+        public async Task<ActionResult> Details(string id, Guid? personId)
+        {
+            if(string.IsNullOrEmpty(id))
+            {
+                return HttpNotFound();
+            }
+            if(personId == null)
+            {
+                return HttpNotFound();
+            }
+
+            using (var client = GetFaceClient())
+            {
+                var model = await client.GetPersonAsync(id, personId.Value);
+                ViewBag.PersonGroupId = id;
+
+                return View(model);
+            }
+        }
+
+        public ActionResult Create(string id)
+        {
+            ViewBag.PersonGroupId = id;
+
+            return View("Edit", new Person());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Person person)
+        {
+            return await Edit(person);
+        }
+
+        public async Task<ActionResult> Edit(string id, Guid personId)
+        {
+            ViewBag.PersonGroupId = id;
+
+            using (var client = GetFaceClient())
+            {
+                var model = await client.GetPersonAsync(id, personId);
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Person person)
+        {
+            var personGroupId = Request.Form["PersonGroupId"];
+            if(string.IsNullOrEmpty(personGroupId))
+            {
+                return HttpNotFound("PersonGroupId is missing");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                ViewBag.PersonGroupId = personGroupId;
+                return View(person);
+            }
+
+            try
+            {
+                using (var client = GetFaceClient())
+                {
+                    if(person.PersonId == Guid.Empty)
+                    {
+                        await client.CreatePersonAsync(personGroupId, person.Name, person.UserData);
+                    }
+                    else
+                    {
+                        await client.UpdatePersonAsync(personGroupId, person.PersonId, person.Name, person.UserData);
+                    }
+
+                    return RedirectToAction("Index", new { id = personGroupId });
+                }
+            }
+            catch (FaceAPIException fex)
+            {
+                ModelState.AddModelError(string.Empty, fex.ErrorMessage);
+            }
+
+            return View(person);
+        }
+    }
+}
