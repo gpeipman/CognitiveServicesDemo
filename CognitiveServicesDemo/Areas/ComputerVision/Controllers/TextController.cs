14c22b8f07e5be2ae95f1fa96daea67343a00bb7
Gunnar <gpeipman@hotmail.com>
2017-09-06 15:36:46 +0300

    added basic text recognizition

--- CognitiveServicesDemo/Areas/ComputerVision/Controllers/TextController.cs
+++ CognitiveServicesDemo/Areas/ComputerVision/Controllers/TextController.cs
@@ -2,31 +2,23 @@
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using CognitiveServicesDemo.Areas.ComputerVision.Models;
-using Microsoft.ProjectOxford.Vision;
-using Microsoft.ProjectOxford.Vision.Contract;
 
 namespace CognitiveServicesDemo.Areas.ComputerVision.Controllers
 {
-    public class DescribeController : ComputerVisionBaseController
+    public class TextController : ComputerVisionBaseController
     {
         public async Task<ActionResult> Index()
         {
             if (Request.HttpMethod == "GET")
             {
-                return View("Index");
+                return View();
             }
 
-            var model = new DescribeImageModel();
-
-            var features = new[]
-            {
-                VisualFeature.Adult, VisualFeature.Categories, VisualFeature.Color, VisualFeature.Description,
-                VisualFeature.Faces, VisualFeature.ImageType, VisualFeature.Tags
-            };
+            var model = new RecognizeTextModel();
 
             await RunOperationOnImage(async stream =>
             {
-                model.Result = await VisionServiceClient.AnalyzeImageAsync(stream, features);
+                model.Results = await VisionServiceClient.RecognizeTextAsync(stream, detectOrientation: false);
             });
 
             await RunOperationOnImage(async stream => {
@@ -36,8 +28,8 @@ namespace CognitiveServicesDemo.Areas.ComputerVision.Controllers
 
                 var base64 = Convert.ToBase64String(bytes);
                 model.ImageDump = String.Format("data:image/png;base64,{0}", base64);
-            });            
-            
+            });
+
             return View(model);
         }
     }
---
+++ CognitiveServicesDemo/Areas/ComputerVision/Models/RecognizeTextModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ProjectOxford.Vision.Contract;
+
+namespace CognitiveServicesDemo.Areas.ComputerVision.Models
+{
+    public class RecognizeTextModel
+    {
+        public OcrResults Results;
+        public string ImageDump;
+    }
+}
